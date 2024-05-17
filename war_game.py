491f08c7ff9745ce959154dbff989d1894bb7bb2
mariusgaida1 <mariusgaida1@gmail.com>
2024-05-17 20:19:37 +0300

    Add create_deck method

--- war_game.py
+++ war_game.py
@@ -6,14 +6,25 @@ class Card:
 
     def __str__(self):
         return f"{self.value} of {self.suite}"
+
+    def __repr__(self) -> str:
+        return f"{self.value} of {self.suite}"
     
 class Deck:
     def __init__(self):
         self.decks = []
-        suites = ['Hearts', 'Clubs', 'Diamonds', 'Spades']
-        values = ['2','3','4','5','6','7','8','9','10','Jack','Queen','King','Ace']
+        self.suites = ['Hearts', 'Clubs', 'Diamonds', 'Spades']
+        self.values = ['2','3','4','5','6','7','8','9','10','Jack','Queen','King','Ace']
+    
+    def create_deck(self):
+        for suite in self.suites:
+            for value in self.values:
+                card = Card(suite, value)
+                self.decks.append(card)
+        return self.decks
 
-
+deck = Deck()
+print(deck.create_deck())
         
 
     
